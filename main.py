e85a2f895972198941acf3204266211b90b0f476
Domin <domin.dober@gmail.com>
2022-03-21 00:23:02 +0100

    Add Neuron class

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Neuron:
+
+    def __init__(self, random_initialization: bool = True) -> None:
+        if random_initialization:
+            self.weight = random.random()
+            self.bias = random.random()
+        else:
+            self.weight = 1
+            self.bias = 1
+
+    def forward(self, value: float) -> float:
+        return value * self.weight + self.bias
+
+    def backward(self):
+        raise Exception('Not implemented yet!')
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    print('Simple neural network project')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
